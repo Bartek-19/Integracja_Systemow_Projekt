0d50cf46aa16b13b549876d01a64ddaf031d23ce
Bartosz Maksymilian Klimiuk <115135789+Bartek-19@users.noreply.github.com>
2025-06-11 21:20:28 +0200

    SoapKlient - pliki programu

--- SoapKlient/Program.cs
+++ SoapKlient/Program.cs
@@ -1,1 +1,29 @@
-.
+﻿using ServiceReference1;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("SOAP Client!");
+        string filePath = "output.xml";
+
+        try
+        {
+            SOAPInterfaceClient client = new SOAPInterfaceClient();
+            var response = await client.getDataAsync();
+            Console.WriteLine("Response received:");
+            
+            string xmlContent = response.Body.@return;
+            File.WriteAllText(filePath, xmlContent);
+
+            Console.WriteLine("Plik XML został zapisany jako: " + Path.GetFullPath(filePath));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Błąd: " + ex.Message);
+        }
+    }
+}
